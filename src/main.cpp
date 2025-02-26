baa83a1dfdbd0252b7cba3b5ff0755b4810d462d
me-bad-dev <44929407+me-bad-dev@users.noreply.github.com>
2025-02-26 20:54:02 +0100

    Update main.cpp
    Added PSRAM detection

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,16 @@
 
 CRGB leds[NUM_LEDS];
 
-void setup() 
-{
+void setup() {
   Serial.begin(115200);
   Serial.print("Avalible flash size: ");
   Serial.print(ESP.getFlashChipSize()/1024/1024);
   Serial.println("MB");
+
+  if (psramFound()) Serial.println("PSRAM: Found");
+  else {
+    Serial.println("PSRAM: Not found");
+  }
 
   FastLED.addLeds<NEOPIXEL, LED_DRIVER_PIN>(leds, NUM_LEDS);
 }
@@ -22,8 +26,7 @@ void loop()
   /* Rainbow effect for onboard addressable RGBW LED */
   /* If the RGBW LED is not working, inspect the soldering pads on the board marked RGB, the pads need to be connected by soldering them together */
 
-  for (int i=0; i<=255;i++) 
-  {
+  for (int i=0; i<=255;i++) {
     leds[0] = CHSV( i, 255, 100);
     FastLED.show();
     delay(15);
